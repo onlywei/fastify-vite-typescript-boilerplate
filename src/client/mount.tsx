43d3774867610e781e91cfc1de7db78d817b758b
Wei Wang <wei@fastmail.com>
2024-05-24 23:36:05 -0400

    add code

---
+++ src/client/mount.tsx
@@ -0,0 +1,3 @@
+const rootElement = document.getElementById('root') || document.createElement('div');
+
+rootElement.innerHTML = 'Hello World'
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { resolve } from 'node:path';
+import Fastify from 'fastify';
+import FastifyVite from '@fastify/vite'
+
+const server = Fastify({ logger: true })
+
+await server.register(FastifyVite, {
+	root: resolve(import.meta.dirname, '../'),
+	dev: process.argv.includes('--dev'),
+	spa: true,
+})
+
+server.get('*', (req, reply) => {
+	return reply.html()
+});
+
+await server.vite.ready()
+
+try {
+	await server.listen({ host: 'localhost', port: 3000 });
+} catch (err) {
+	server.log.error(err);
+	process.exit(1);
+}
